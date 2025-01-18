28c865e65ddaabde37b51b3be998c5d97ada2eae
Omar Swailam <omarswailam@outlook.com>
2025-01-19 00:19:39 +0200

    update styles

--- main.js
+++ main.js
@@ -1,71 +1,77 @@
-document.addEventListener('DOMContentLoaded', function() {
-// List of sentences
-var _CONTENT = [
-    'Backend developer',
-    'DevOps enthusiast',
-    'Pythonist',
-    'Problem solver',
-];
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("theme-toggle");
 
-// Current sentence being processed
-var _PART = 0;
+    toggleButton.addEventListener("click", () => {
+      document.documentElement.classList.toggle("light-mode");
+    });
+  });
 
-// Character number of the current sentence being processed
-var _PART_INDEX = 0;
+  // List of sentences
+  var _CONTENT = [
+    "Backend developer",
+    "DevOps enthusiast",
+    "Pythonist",
+    "Problem solver",
+  ];
 
-// Holds the handle returned from setInterval
-var _INTERVAL_VAL;
+  // Current sentence being processed
+  var _PART = 0;
 
-// Element that holds the text
-var _ELEMENT = document.querySelector("#text");
+  // Character number of the current sentence being processed
+  var _PART_INDEX = 0;
 
-// Cursor element
-var _CURSOR = document.querySelector("#cursor");
+  // Holds the handle returned from setInterval
+  var _INTERVAL_VAL;
 
-// Implements typing effect
-function Type() {
-	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX++;
+  // Element that holds the text
+  var _ELEMENT = document.querySelector("#text");
 
-	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
-		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 70);
-		}, 1500);
-	}
-}
+  // Cursor element
+  var _CURSOR = document.querySelector("#cursor");
 
-// Implements deleting effect
-function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX--;
+  // Implements typing effect
+  function Type() {
+    // Get substring with 1 characater added
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX++;
 
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
+    // If full sentence has been displayed then start to delete the sentence after some time
+    if (text === _CONTENT[_PART]) {
+      clearInterval(_INTERVAL_VAL);
+      setTimeout(function () {
+        _INTERVAL_VAL = setInterval(Delete, 70);
+      }, 1500);
+    }
+  }
 
-		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
-			_PART = 0;
-		else
-			_PART++;
+  // Implements deleting effect
+  function Delete() {
+    // Get substring with 1 characater deleted
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX--;
 
-		_PART_INDEX = 0;
+    // If sentence has been deleted then start to display the next sentence
+    if (text === "") {
+      clearInterval(_INTERVAL_VAL);
 
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Type, 150);
-		}, 200);
-	}
-}
+      // If current sentence was last then display the first one, else move to the next
+      if (_PART == _CONTENT.length - 1) _PART = 0;
+      else _PART++;
 
-// Start the typing effect on load
-if(_ELEMENT) {
-	_INTERVAL_VAL = setInterval(Type, 100);
-}
+      _PART_INDEX = 0;
+
+      // Start to display the next sentence after some time
+      setTimeout(function () {
+        _INTERVAL_VAL = setInterval(Type, 150);
+      }, 200);
+    }
+  }
+
+  // Start the typing effect on load
+  if (_ELEMENT) {
+    _INTERVAL_VAL = setInterval(Type, 100);
+  }
 });
